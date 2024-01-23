03e3101d07d59b9ab26b13f860426542f17044e9
VahidR <vahid.rafiei@gmail.com>
2024-01-23 10:51:29 +0100

    token.go -> Some exploratory comment for future references and future hacking on interpreters.

--- token/token.go
+++ token/token.go
@@ -1,10 +1,12 @@
 package token
 
+// TokenType Let's have a formal type for tokens. In real-world, they can be much more efficient,
+// but let's have it as string for now. We can also easily print it which is useful for debugging.
 type TokenType string
 
 type Token struct {
 	Type    TokenType
-	Literal string
+	Literal string // this is the literal value of the token
 }
 
 const (
